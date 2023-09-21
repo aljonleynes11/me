fa2b57016efff9354f383bf032be70cd3b93e44f
aljonleynes11 <aljonleynes11@gmail.com>
2023-09-21 18:12:06 +0800

    1 page app

--- script.js
+++ script.js
@@ -1,47 +1,10 @@
 
-const element1 = document.querySelector('.section1');
-const element2 = document.querySelector('.section2');
-const element3 = document.querySelector('.section3');
-const els = [
-    element1, element2, element3,
-]
-
-function isElementInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    const height = (window.innerHeight || document.documentElement.clientHeight);
-    const width = (window.innerWidth || document.documentElement.clientWidth);
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= height &&
-        rect.right <= width
-    );
-}
-
-function checkElementVisibility() {
-    els.forEach((el) => {
-        if (isElementInViewport(el)) {
-            el.classList.add('visible'); // Add a class to trigger the fade-in animation
-        } else {
-            el.classList.remove('visible'); // Remove the class to hide the element
-        }
-    });
-}
-
-checkElementVisibility();
-
-const scrollContainer = document.querySelector('.scroll-snap-container'); // Replace with the appropriate scroll container selector
-scrollContainer.addEventListener('scroll', checkElementVisibility);
-
-element1.addEventListener('mouseover', () => {
-    console.log('Mouse over .section1');
-});
 
 function data() {
     return {
         name: "Aljon Leynes",
         role: "Full Stack Mobile Developer",
-        wave: "",
+        state: "about",
         specialization: [
             "Flutter",
             "Laravel"
@@ -63,14 +26,6 @@ function data() {
                 "name": "iamcare",
                 "path": "assets/images/projects/iamcare.png",
             },
-            // {
-            //     "name": "Bukka Services",
-            //     "path": "assets/images/projects/bukkaservices.jpg",
-            // },
-            // {
-            //     "name": "Bukka Waste",
-            //     "path": "assets/images/projects/bukkawaste.png",
-            // },
             {
                 "name": "phreviewer",
                 "path": "assets/images/projects/phreviewer.png",
@@ -88,7 +43,7 @@ function data() {
                 "path": "assets/images/projects/wheelcheck.png",
             },
         ],
-        "techStack": [
+        techStack: [
 
             {
                 "name": "Flutter",
@@ -143,7 +98,7 @@ function data() {
                 "path": "assets/images/tech_stack/tailwind.jpg",
             },
         ],
-        "socials": [
+        socials: [
             {
                 "name": "linkedin",
                 "path": "assets/images/socials/linkedin.png",
@@ -158,9 +113,19 @@ function data() {
             },
 
         ],
-
         clickEm() {
             window.location.href = "mailto:aljonleynes11@gmail.com?subject=Hi,%20Aljon&body=Let's%20connect.";
+        },
+        setState(string) {
+            this.state = 'loading';
+            setTimeout(() => {
+                this.goState(string);
+            }, 500);
+        },
+        goState(string) {
+            if (this.state != string) {
+                this.state = string;
+            }
         }
     }
 }
