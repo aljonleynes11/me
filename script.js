9c6e63428f27f84359b216e7a81cc24d13197fe6
aljonleynes11 <aljonleynes11@gmail.com>
2023-12-14 16:04:58 +0800

    fix/particles

--- script.js
+++ script.js
@@ -1,4 +1,116 @@
 
+window.onload = function () {
+    particlesJS("particles-js", {
+        particles: {
+            number: {
+                value: 5,
+                density: {
+                    enable: true,
+                    value_area: 800,
+                },
+            },
+            color: {
+                value: "#ffe100",
+            },
+            shape: {
+                type: "circle",
+                stroke: {
+                    width: 0,
+                    color: "#000000",
+                },
+                polygon: {
+                    nb_sides: 3,
+                },
+                image: {
+                    src: "img/github.svg",
+                    width: 100,
+                    height: 100,
+                },
+            },
+            opacity: {
+                value: 0.312665351868777,
+                random: true,
+                anim: {
+                    enable: false,
+                    speed: 1,
+                    opacity_min: 0.1,
+                    sync: false,
+                },
+            },
+            size: {
+                value: 20.042650760819036,
+                random: true,
+                anim: {
+                    enable: true,
+                    speed: 40,
+                    size_min: 30.858934067451116,
+                    sync: false,
+                },
+            },
+            line_linked: {
+                enable: true,
+                distance: 0,
+                color: "#f6e71d",
+                opacity: 0.4,
+                width: 1,
+            },
+            move: {
+                enable: true,
+                speed: 9.620472365193136,
+                direction: "none",
+                random: true,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                    enable: false,
+                    rotateX: 600,
+                    rotateY: 1200,
+                },
+            },
+        },
+        interactivity: {
+            detect_on: "canvas",
+            events: {
+                onhover: {
+                    enable: true,
+                    mode: "repulse",
+                },
+                onclick: {
+                    enable: false,
+                    mode: "repulse",
+                },
+                resize: true,
+            },
+            modes: {
+                grab: {
+                    distance: 400,
+                    line_linked: {
+                        opacity: 1,
+                    },
+                },
+                bubble: {
+                    distance: 400,
+                    size: 40,
+                    duration: 2,
+                    opacity: 8,
+                    speed: 3,
+                },
+                repulse: {
+                    distance: 200,
+                    duration: 0.4,
+                },
+                push: {
+                    particles_nb: 4,
+                },
+                remove: {
+                    particles_nb: 2,
+                },
+            },
+        },
+        retina_detect: true,
+    });
+};
 
 function data() {
     return {
@@ -118,23 +230,29 @@ function data() {
             {
                 "name": "linkedin",
                 "path": "assets/images/socials/linkedin.png",
+                "link": "https://www.linkedin.com/in/ajleynes11/"
             },
             {
                 "name": "github",
                 "path": "assets/images/socials/github.jpg",
+                "link": "https://github.com/aljonleynes11",
             },
             {
                 "name": "facebook",
                 "path": "assets/images/socials/facebook.png",
+                "link": "https://fb.com/aljonleynes11"
             },
 
         ],
+        clickSocials(uri){
+            window.open(uri, '_blank');
+        },
         clickEm() {
             window.location.href = "mailto:aljonleynes11@gmail.com?subject=Hi,%20Aljon&body=Let's%20connect.";
         },
         setState(string, el) {
             this.goState(string);
-           
+
         },
         goState(string) {
             if (this.state != string) {
